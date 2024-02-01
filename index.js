a796cf8d26883806da7dd724bd70ccb2af1465d7
Andrew Labutin <labutin.99@bk.ru>
2024-02-01 18:02:27 +0700

    финальный макет 1.6

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+import './scss/main.scss'
+
 const swiper = new Swiper('.swiper', {
   // Optional parameters
+  slidesPerView: 'auto',
   direction: 'horizontal',
   loop: true,
   spaceBetween: 16,
@@ -7,35 +10,230 @@ const swiper = new Swiper('.swiper', {
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
-    clickable: true,
-  },
-});
+    clickable: true
+  }
+})
 
-const button = document.querySelector('.show-close');
-const icon = document.querySelector('.icon-swiper');
-const text = document.querySelector('.text-swiper');
-const viewsonic = document.querySelector('.viewsonic');
-const sony = document.querySelector('.sony');
-const sumsung2 = document.querySelector('.sumsung2');
-const lenovo2 = document.querySelector('.lenovo2');
-const apple2 = document.querySelector('.apple2');
+// обработчик клика для иконок брендов
+const button = document.querySelector('.show-close')
+const icon = document.querySelector('.icon-swiper')
+const text = document.querySelector('.text-swiper')
+const viewsonic = document.querySelector('.viewsonic')
+const sony = document.querySelector('.sony')
+const sumsung2 = document.querySelector('.sumsung2')
+const lenovo2 = document.querySelector('.lenovo2')
+const apple2 = document.querySelector('.apple2')
 
 button.addEventListener('click', function () {
   if (text.textContent === 'Показать все') {
-    text.textContent = 'Скрыть';
-    icon.style.transform = 'rotate(180deg)';
-    viewsonic.classList.remove('none1');
-    sony.classList.remove('none1');
-    sumsung2.classList.remove('none');
-    lenovo2.classList.remove('none');
-    apple2.classList.remove('none');
+    text.textContent = 'Скрыть'
+    icon.style.transform = 'rotate(180deg)'
+    viewsonic.classList.remove('none1')
+    sony.classList.remove('none1')
+    sumsung2.classList.remove('none')
+    lenovo2.classList.remove('none')
+    apple2.classList.remove('none')
   } else {
-    text.textContent = 'Показать все';
-    icon.style.transform = 'rotate(360deg)';
-    viewsonic.classList.add('none1');
-    sony.classList.add('none1');
-    sumsung2.classList.add('none');
-    lenovo2.classList.add('none');
-    apple2.classList.add('none');
+    text.textContent = 'Показать все'
+    icon.style.transform = 'rotate(360deg)'
+    viewsonic.classList.add('none1')
+    sony.classList.add('none1')
+    sumsung2.classList.add('none')
+    lenovo2.classList.add('none')
+    apple2.classList.add('none')
   }
-});
+})
+// обработчик кнопки с текстом после header
+const textMain = document.querySelector('.main-content__text__secondPart')
+const buttonText = document.querySelector('.main-contentButton')
+const iconText = document.querySelector('.main-contentIcon')
+const readText = document.querySelector('.main-content__read')
+
+buttonText.addEventListener('click', function () {
+  if (readText.textContent === 'Читать далее') {
+    readText.textContent = 'Скрыть'
+    textMain.classList.remove('hidden')
+    iconText.style.transform = 'rotate(180deg)'
+  } else {
+    readText.textContent = 'Читать далее'
+    textMain.classList.add('hidden')
+    iconText.style.transform = 'rotate(0deg)'
+  }
+})
+
+const repairButton = document.querySelector('.repair-list__button')
+const repairItems = document.getElementsByClassName('repair-list__item')
+const repairIcon = document.querySelector('.repair-icon')
+const repairText = document.querySelector('.repair-text')
+const arrRepair = Array.from(repairItems)
+
+repairButton.addEventListener('click', function () {
+  if (
+    repairText.textContent === 'Показать все' &&
+    window.innerWidth >= 769 &&
+    window.innerWidth <= 1120
+  ) {
+    repairIcon.style.transform = 'rotate(180deg)'
+    repairText.textContent = 'Скрыть'
+    arrRepair.slice(-3).forEach((item) => {
+      item.classList.remove('hidden')
+    })
+  } else if (
+    repairText.textContent === 'Скрыть' &&
+    window.innerWidth >= 769 &&
+    window.innerWidth <= 1120
+  ) {
+    repairIcon.style.transform = 'rotate(0deg)'
+    repairText.textContent = 'Показать все'
+    arrRepair.slice(-3).forEach((item) => {
+      item.classList.add('hidden')
+    })
+  } else if (
+    repairText.textContent === 'Показать все' &&
+    window.innerWidth >= 1121
+  ) {
+    repairIcon.style.transform = 'rotate(180deg)'
+    repairText.textContent = 'Скрыть'
+    arrRepair.slice(4).forEach((item) => {
+      item.classList.remove('hidden')
+    })
+  } else if (repairText.textContent === 'Скрыть' && window.innerWidth >= 1121) {
+    arrRepair.slice(4).forEach((item) => {
+      item.classList.add('hidden')
+    })
+    repairIcon.style.transform = 'rotate(0deg)'
+    repairText.textContent = 'Показать все'
+  }
+})
+
+window.addEventListener('resize', function () {
+  if (window.innerWidth >= 1121 && repairText.textContent === 'Показать все') {
+    arrRepair[3].classList.remove('hidden')
+  } else if (
+    window.innerWidth >= 769 &&
+    window.innerWidth <= 1120 &&
+    repairText.textContent === 'Показать все'
+  ) {
+    arrRepair[3].classList.add('hidden')
+  }
+})
+
+if (window.innerWidth >= 1121) {
+  arrRepair[3].classList.remove('hidden')
+} else if (window.innerWidth >= 769 && window.innerWidth <= 1120) {
+  arrRepair[3].classList.add('hidden')
+}
+
+const layerOpacity = document.querySelector('.sidebar-blur')
+const leftSidebar = document.querySelector('.aside')
+
+document.querySelector('.aside-button').addEventListener('click', function () {
+  if (window.innerWidth <= 1120) {
+    leftSidebar.style.transform = 'translateX(360px)'
+    layerOpacity.classList.add('left-sidebar')
+  }
+})
+
+document.querySelector('.icon-quit').addEventListener('click', function () {
+  if (window.innerWidth <= 1120) {
+    leftSidebar.style.transform = 'translateX(-360px)'
+    layerOpacity.classList.remove('left-sidebar')
+  }
+})
+
+layerOpacity.addEventListener('click', function () {
+  if (window.innerWidth <= 1120) {
+    layerOpacity.classList.remove('left-sidebar')
+    leftSidebar.style.transform = 'translateX(-360px)'
+  } else if (window.innerWidth >= 1121) {
+    layerOpacity.classList.remove('left-sidebar')
+    rightsidebar.style.transform = 'translateX(520px)'
+  }
+})
+
+const chatbtn = document.querySelector('.chatbtn')
+const rightsidebar = document.querySelector('.asideModalFeedback')
+const quitbtn = document.querySelector('.quitbtn')
+const iconchat768 = document.querySelector('.icon-chat')
+const blurRight = document.querySelector('.sidebar-blur-right')
+
+chatbtn.addEventListener('click', function () {
+  if (window.innerWidth <= 768 || window.innerWidth <= 1120) {
+    rightsidebar.style.transform = 'translateX(-360px)'
+    rightsidebar.style.width = '100%'
+  } else if (window.innerWidth >= 1121) {
+    blurRight.classList.add('left-sidebar')
+    rightsidebar.style.transform = 'translateX(-520px)'
+  }
+})
+
+quitbtn.addEventListener('click', function () {
+  if (window.innerWidth <= 768 || window.innerWidth <= 1120) {
+    rightsidebar.style.width = '320px'
+    rightsidebar.style.transform = 'translateX(360px)'
+    blurRight.classList.remove('left-sidebar')
+  } else if (window.innerWidth >= 1121) {
+    blurRight.classList.remove('left-sidebar')
+    rightsidebar.style.transform = 'translateX(520px)'
+  }
+})
+
+iconchat768.addEventListener('click', function () {
+  if (window.innerWidth <= 1120) {
+    rightsidebar.style.transform = 'translateX(-360px)'
+    blurRight.classList.add('left-sidebar')
+  }
+})
+
+blurRight.addEventListener('click', function () {
+  if (window.innerWidth <= 1120) {
+    blurRight.classList.remove('left-sidebar')
+    rightsidebar.style.transform = 'translateX(360px)'
+  } else if (window.innerWidth >= 1121) {
+    blurRight.classList.remove('left-sidebar')
+    rightsidebar.style.transform = 'translateX(520px)'
+  }
+})
+
+const callbtn = document.querySelector('.callbtn')
+const callbtn768 = document.querySelector('.icon-call')
+const asidecallback = document.querySelector('.call')
+const callquit = document.querySelector('.call-quit')
+const callbackOpacity = document.querySelector('.sidebar-callbackOpacity')
+
+callbtn.addEventListener('click', function () {
+  if (window.innerWidth <= 1120) {
+    asidecallback.style.transform = 'translateX(-360px)'
+    asidecallback.style.width = '100%'
+  } else if (window.innerWidth >= 1121) {
+    asidecallback.style.transform = 'translateX(-520px)'
+    callbackOpacity.classList.add('left-sidebar')
+  }
+})
+
+callbtn768.addEventListener('click', function () {
+  if (window.innerWidth <= 1120) {
+    asidecallback.style.transform = 'translateX(-360px)'
+    callbackOpacity.classList.add('left-sidebar')
+  }
+})
+
+callquit.addEventListener('click', function () {
+  if (window.innerWidth <= 1120) {
+    asidecallback.style.width = '320px'
+    asidecallback.style.transform = 'translateX(360px)'
+    callbackOpacity.classList.remove('left-sidebar')
+  } else if (window.innerWidth >= 1121) {
+    asidecallback.style.transform = 'translateX(520px)'
+    callbackOpacity.classList.remove('left-sidebar')
+  }
+})
+
+callbackOpacity.addEventListener('click', function () {
+  callbackOpacity.classList.remove('left-sidebar')
+  if (window.innerWidth <= 1120) {
+    asidecallback.style.transform = 'translateX(360px)'
+  } else if (window.innerWidth >= 1121) {
+    asidecallback.style.transform = 'translateX(520px)'
+  }
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,91 +1,48 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 module.exports = {
-  // Входной файл
-  entry: ['./src/js/index.js'],
-
-  // Выходной файл
+  mode: 'development',
+  stats: {
+    children: true
+  },
+  entry: {
+    main: path.resolve(__dirname, './index.js')
+  },
   output: {
-    filename: './js/bundle.js'
+    path: path.resolve(__dirname, './dist'),
+    filename: 'dist.bundle.js'
   },
 
-  // Source maps для удобства отладки
-  devtool: 'source-map',
-
-  module: {
-    rules: [
-      // Транспилируем js с babel
-      {
-        test: /\.js$/,
-        include: path.resolve(__dirname, 'src/js'),
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './index.html',
+      chunks: ['main']
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'assets/images',
+          to: 'assets/images',
+          globOptions: {
+            ignore: ['*.DS_Store']
           }
         }
-      },
+      ]
+    })
 
-      // Компилируем SCSS в CSS
+    //new CleanWebpackPlugin()
+  ],
+  module: {
+    rules: [
+      { test: /\.(?:ico|gif|png|jpg|jpeg)$/i, type: 'asset/resource' },
       {
-        test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader, // Extract css to separate file
-          'css-loader', // translates CSS into CommonJS
-          'postcss-loader', // parse CSS and add vendor prefixes to CSS rules
-          'sass-loader' // compiles Sass to CSS, using Node Sass by default
-        ]
-      },
-
-      // Подключаем шрифты из css
-      {
-        test: /\.(eot|ttf|woff|woff2)$/,
-        use: [
-          {
-            loader: 'file-loader?name=./fonts/[name].[ext]'
-          }
-        ]
-      },
-
-      // Подключаем картинки из css
-      {
-        test: /\.(svg|png|jpg|jpeg|webp)$/,
-        use: [
-          {
-            loader: 'file-loader?name=./static/[name].[ext]'
-          }
-        ]
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
       }
     ]
-  },
-  plugins: [
-    // Подключаем файл html, стили и скрипты встроятся автоматически
-    new HtmlWebpackPlugin({
-      title: 'Webpack 4 Starter',
-      template: '/src/index.html',
-      inject: true,
-      minify: {
-        removeComments: true,
-        collapseWhitespace: false
-      },
-      filename: 'index.html'
-    }),
-
-    // Кладем стили в отдельный файлик
-    new MiniCssExtractPlugin({
-      filename: 'style.css'
-    }),
-
-    // Копируем картинки
-    new CopyWebpackPlugin([
-      {
-        from: './src/img',
-        to: 'img'
-      }
-    ])
-  ]
+  }
 }
